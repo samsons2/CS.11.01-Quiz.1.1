324468309814c2032e04e387328e85c26967e8a0
samsonsuen <samsons2026@student.cis.edu.hk>
2023-09-13 09:55:29 +0800

    quiz done

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,21 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner =new Scanner(System.in);
+        int age;
+        String firstName;
+        String favouriteFood;
+        System.out.println("Please enter your age:");
+        age = Integer.parseInt(scanner.nextLine());
+        System.out.println("Please enter your name:");
+        firstName = scanner.nextLine();
+        System.out.println("Please enter your favourite food");
+        favouriteFood = scanner.nextLine();
+        System.out.println("First name:"+firstName+"\n\nAge:"+age+"\n\nFavourite food:"+favouriteFood);
+
+
+
+
 
 
 
